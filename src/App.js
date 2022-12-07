2ca024a9cf758afbfd080652d909cb0d04dce5f9
root <root@DESKTOP-H7TOAET.localdomain>
2022-12-07 06:52:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Dhanashri ! You are AWESOME. You can achieve anything you want
+	  Improve your knowledge and you will fly high
         </p>
         <a
           className="App-link"
